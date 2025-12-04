1ddd2f641132c9f5887f435e0da5ab94682f2bfb
Martin <marrty.h.1992@gmail.com>
2025-12-04 18:31:45 +0100

    Initial Commit

---
+++ Items/ItemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemsApp.swift
+//  Items
+//
+//  Created by Martin Hrbáček on 04.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
